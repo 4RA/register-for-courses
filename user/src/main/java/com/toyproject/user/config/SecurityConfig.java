1c44e4dc63736c90cce8c5af33b34aa913012cc7
cwkim723 <jerry.pref@gmail.com>
2022-04-02 13:56:42 +0900

    feat: 스프링 시큐리티 설정 중

---
+++ user/src/main/java/com/toyproject/user/config/SecurityConfig.java
@@ -0,0 +1,91 @@
+package com.toyproject.user.config;
+
+import com.toyproject.user.jwt.JwtAccessDeniedHandler;
+import com.toyproject.user.jwt.JwtAuthenticationEntryPoint;
+import com.toyproject.user.jwt.JwtSecurityConfig;
+import com.toyproject.user.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity // 기본적인 웹 보안 활성화
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드 단위로 추가
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // WebSecurityConfigurer를 implements하거나 WebSecurityConfigurerAdapter를 extends하는 방법 존재
+
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            CorsFilter corsFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.corsFilter = corsFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/favicon.ico"
+                        ,"/h2-console/**"
+                        ,"/error"
+                        ,"/resources/**"
+                );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .antMatchers("/main").authenticated() // "/"에 대한 요청은 인증없이 접근 허용 -> 프로젝트 실행 시 메인 페이지(http://localhost:9000/)는 누구든 들어갈 수 있도록
+                .anyRequest().permitAll() // 나머지 요청들은 모두 인증 받아야 함
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        http.cors().and();
+    }
+
+
+}
